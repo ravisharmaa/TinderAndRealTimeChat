762b09f1a4d8fba1accdb0514f687cc2e768b27f
ravisharmaa <bastola.ravi@gmail.com>
2020-03-07 13:20:08 +0545

    Lesson 2. Add Attributed String and Complete the Introduction Label

--- RealtimeChatAndTinderClone/ViewController.swift
+++ RealtimeChatAndTinderClone/ViewController.swift
@@ -1,12 +1,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     //MARK:- Properties
     
     lazy var createAccountLabel: UILabel = {
         let label = UILabel()
-        label.text = "Create a new account"
         return label
     }()
     
@@ -48,9 +47,52 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemRed
+        view.backgroundColor = .systemBackground
+        
+        setupUI()
     }
-
-
+    
+    func setupUI() {
+        let title = "Create a new account"
+        let subtitle = "\n\n This is the subtitle which was meant to be a subtitle and ended up being a subtitle which to much more to be a subtitle."
+        
+        //NSAttributed String
+        
+        let attributedText = NSMutableAttributedString(string:  title, attributes: [
+            NSAttributedString.Key.font: UIFont.init(name: "Didot", size: 28)!,
+            NSAttributedString.Key.foregroundColor : UIColor.black,
+        ])
+        
+        let attributedSubtitle = NSMutableAttributedString(string: subtitle, attributes: [
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16),
+            NSAttributedString.Key.foregroundColor: UIColor(white: 0, alpha: 0.45)
+        ])
+        
+        attributedText.append(attributedSubtitle)
+        
+        let paragraphStyle = NSMutableParagraphStyle()
+        
+        paragraphStyle.lineSpacing = 5
+        
+        attributedText.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragraphStyle, range: NSMakeRange(0, attributedText.length))
+        
+        createAccountLabel.numberOfLines = 0
+        
+        createAccountLabel.lineBreakMode = .byCharWrapping
+        
+        createAccountLabel.attributedText = attributedText
+        
+        view.addSubview(createAccountLabel)
+        
+        createAccountLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            createAccountLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            createAccountLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            createAccountLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20)
+        ])
+    }
+    
+    
 }
 
